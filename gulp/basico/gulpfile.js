4a2457a4ec667e75ba28fe97989369abb71405b5
robsongarola <149022651+robsongarola@users.noreply.github.com>
2024-04-02 20:15:36 -0300

    010424

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,11 +1,38 @@
 const gulp = require('gulp');
-const series = gulp.series
+const {series, parallel} = require('gulp')
 
-function copiar(cb){
 
-    console.log('tarefa de copiar !')
+function antes1(cb){ // task
+
+    console.log('tarefa antes1 !')
+
+    return cb()
+}
+function antes2(cb){ // task
+
+    console.log('tarefa antes2 !')
 
     return cb()
 }
 
-module.exports.default= series(copiar)
+function copiar(cb){ // task
+
+   // gulp.src(['pastaA/arquivo1.txt','pastaA/arquivo2.txt']) // configuração de entrada
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB')) //pipeLine  -- faz uma transformação (podemos ter varias). Nesse caso cria a pasta B
+        // e copia para pasta criada
+
+    return cb()
+}
+function fim(cb){ // task
+
+    console.log('tarefa fim!')
+
+    return cb()
+}
+
+module.exports.default= series(
+   parallel(antes1, antes2),    
+    copiar,
+    fim
+    )
