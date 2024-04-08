3e292bc1f08ee5c2fddae14e4188fcddff7dc90f
robsongarola <149022651+robsongarola@users.noreply.github.com>
2024-04-07 21:41:22 -0300

    0704241

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,18 +1,29 @@
 const gulp = require('gulp');
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
+
+
+function servidor(){
+
+    return gulp.src('build')
+            .pipe(webserver({
+                port: 8080,
+                open: true,
+                livereload: true
+            }))
+}
 
 function monitorarArquivos(cb){
 
+    watch('src/**/*.html',()=> gulp.series('appHTML')())
     return cb()
 }
 
-function servidor(cb){
 
-    return cb()
-}
 
 module.exports={
 
-    monitorarArquivos,
-    servidor 
+    servidor,
+    monitorarArquivos
 }
