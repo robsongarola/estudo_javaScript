29aa53287f122b904c89eaf4bd8acad8fdfd0cd2
robsongarola <149022651+robsongarola@users.noreply.github.com>
2024-04-07 21:40:42 -0300

    070424

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,25 +1,42 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const unglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
+const uglifycss= require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
-function appHTML(cb){
+function appHTML(){
 
-    return cb()
+    return gulp.src('src/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))  
+        .pipe(gulp.dest('build'))
 }
-function appJS(cb){
+function appCSS(){
 
-    return cb()
+    return gulp.src('src/assets/sass/index.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(uglifycss({"uglyComments": true}))
+            .pipe(concat('app.min.css'))
+            .pipe(gulp.dest('build/assets/css'))
 }
-function appCSS(cb){
+function appJS(){
 
-    return cb()
+    return gulp.src('src/assets/js/**/*.js')
+            .pipe(babel({presets:["ENV"]}))
+            .pipe(unglify())
+            .pipe(concat('app.min.js'))
+            .pipe(gulp.dest('build/assets/js'))
 }
-function appIMG(cb){
+function appIMG(){
 
-    return cb()
+    return gulp.src('src/assets/imgs/**/*.*')
+            .pipe(gulp.dest('build/assets/imgs'))
 }
 
 module.exports={
     appHTML,
-    appJS,
     appCSS,
+    appJS,
     appIMG
 }
--- gulp/spa/gulpfile.js
+++ gulp/spa/gulpfile.js
@@ -1,7 +1,7 @@
 const {series, parallel}= require('gulp');
 const gulp = require('gulp');
 
-const {appHTML, appJS, appCSS, appIMG} = require('./gulpTasks/app');
+const {appHTML, appCSS, appJS, appIMG} = require('./gulpTasks/app');
 const {depsCSS, depsFONTS}= require('./gulpTasks/deps');
 const {monitorarArquivos, servidor}= require('./gulpTasks/servidor');
 
@@ -10,7 +10,7 @@ const {monitorarArquivos, servidor}= require('./gulpTasks/servidor');
 
 exports.default=series(
     parallel(
-        series(appHTML, appJS, appCSS, appIMG),
+        series(appHTML, appCSS, appJS, appIMG),
         series(depsCSS, depsFONTS)
         ),
     servidor,
